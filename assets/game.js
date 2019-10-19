ed8de085c471a35daad18bf6a52feb191ca34545
Pary Ly <paleepary@aol.com>
2019-10-19 16:53:47 -0700

    added classes to the input in html, and js code to log in the user selection

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,5 @@
 // TIMER
-var timer = 45;
+var timer = 31;
 var intervalId;
 
 // START THE GAME
@@ -8,49 +8,43 @@ function start() {
         clearInterval(intervalId);
         intervalId = setInterval (decrement, 1000);
         console.log("starting the game")
-
     });
 
-    function decrement(){
-        timer--;
-        $("#timeclock").text(timer);
-        if (timer === 0){
-            // alert("Times Up!!!");
-            stop();
-        };
-    };
-    decrement();
-
-    function stop(){
-        clearInterval(intervalId);
+function decrement(){
+    timer--;
+    $("#timeclock").text(timer);
+    if (timer === 0){
+        // alert("Times Up!!!");
+        stop();
     };
 };
+decrement();
+
+function stop(){
+    clearInterval(intervalId);
+};
+};
 start();
-
-
-
 
 // LIST OF QUESTIONS
 var questionList = [
 {   question: "Which city is nickname, 'The Big Apple'?",
     choices: ["Los Angeles ", "Paris ", "New York City ", "Phoenix "],
-    answer: "New York City "
+
 },
 {   question: "Who was the first president of the United States of America?",
     choices: ["Trump", "Washington", "Obama", "Lincoln"],
-    anwser: "washington"
 },
 {   question: "What year was the first phone invented?",
     choices: ["1900", "2010", "1945", "1876"],
-    answer: "1876"
 },
 {   question: "Which team won the first ever NFL Superbowl?",
-    choices: ["Raiders", "Packers", "Cowboys", "lions"],
-    answer: "Packers"
+    choices: ["Raiders", "Packers", "Cowboys", "Lions"],
 },
 {   question: "How much is 1 kilogram in a pound?",
     choices: ["5.5 lbs", "1.2 lbs", "2.2 lbs", "2.5 lbs"],
-    answer: "2.2 lbs"
+},
+{   answer: ["new york city", "washington", "1876", "packers", "2.2 lbs"]
 },
 ];
 
@@ -96,22 +90,77 @@ function displayQuiz(){
     
 displayQuiz();
 
+var score = 0;
 
-// CHECK USERS SELECTIONS
-function checkAnswer (){
- 
-}
+$(".userInput1").on("click", function(event){
+    var userSelection = $('input[class=userInput1]:checked').val();
+    console.log(userSelection);
+    for (var a = 0; a < questionList[5].answer.length; a++){
+        var answerlist = questionList[5].answer[a];
+        // console.log(answerlist);
+        if (userSelection === answerlist){
+            score++;
+            console.log(score);
+        };
+    };
+});
+$(".userInput2").on("click", function(event){
+    var userSelection = $('input[class=userInput2]:checked').val();
+    console.log(userSelection);
+    for (var a = 0; a < questionList[5].answer.length; a++){
+        var answerlist = questionList[5].answer[a];
+        // console.log(answerlist);
+        if (userSelection === answerlist){
+            score++;
+            console.log(score);
+        };
+    };
+});
+$(".userInput3").on("click", function(event){
+    var userSelection = $('input[class=userInput3]:checked').val();
+    console.log(userSelection);
+    for (var a = 0; a < questionList[5].answer.length; a++){
+        var answerlist = questionList[5].answer[a];
+        // console.log(answerlist);
+        if (userSelection === answerlist){
+            score++;
+            console.log(score);
+        };
+    };
+});
+$(".userInput4").on("click", function(event){
+    var userSelection = $('input[class=userInput4]:checked').val();
+    console.log(userSelection);
+    for (var a = 0; a < questionList[5].answer.length; a++){
+        var answerlist = questionList[5].answer[a];
+        // console.log(answerlist);
+        if (userSelection === answerlist){
+            score++;
+            console.log(score);
+        };
+    };
+});
+$(".userInput5").on("click", function(event){
+    var userSelection = $('input[class=userInput5]:checked').val();
+    console.log(userSelection);
+    for (var a = 0; a < questionList[5].answer.length; a++){
+        var answerlist = questionList[5].answer[a];
+        // console.log(answerlist);
+        if (userSelection === answerlist){
+            score++;
+            console.log(score);
+        };
+    };
+});
 
-checkAnswer();
 
 // FINISH THE GAME WHEN CLICKING ON THE DONE BUTTON
 function finish(){
     $("#doneButton").on("click",function(){
         clearInterval(intervalId);
         console.log("finish");
-        // $("#quizContainer").hide();
-        
-
+        $("#quizContainer").hide();
+        $("#result").append("<div> Your Score:  </div>")
     });
 };
 finish();
